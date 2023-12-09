dd6d0805faada09a7d965390ffa7011bd6e7a2e8
tanim arefin <ttanimarefin@gmail.com>
2023-12-10 00:07:01 +0600

    variable

---
+++ basic/variable.php
@@ -0,0 +1,25 @@
+<?php
+//variable
+$country= "bangladesh" ;
+$age= 24;
+$myName= "Tanim";
+
+echo 'hi i am' ." ".$myName.".". 'my age is'." ".$age;
+echo"\n";
+
+//variable value changeable 
+$task='read';
+echo $task;
+echo"\n";
+$task='write';
+echo $task;
+echo"\n";
+
+
+//constant
+define('PI',3.1416);
+echo 'value '.PI;
+
+
+
+?>
