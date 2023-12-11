f33fb458d76b202f79d63f2ef1edc827cbda0a9b
tanim arefin <ttanimarefin@gmail.com>
2023-12-11 13:08:46 +0600

    number system

--- basic/variable.php
+++ basic/variable.php
@@ -85,6 +85,23 @@ $num3=(5+1) * 5 -3;
 echo $num3;
 echo"\n";
 
+//number system
+
+$d=12; //dec
+$o=012; //oct
+$h=0x2B;
+
+printf("The Number is %d and %d %d",$d,$o,$h);
+echo"\n";
+printf("The binary of  is %b",$d); 
+echo"\n";
+printf("The hexadecimal of %d is %x",12,1245); 
+echo"\n";
+printf("The octal of %d is %o",12,12); 
+echo"\n";
+ 
+
+
 
 
 
