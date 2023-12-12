07725a7f0c2a2a97159c81f591a752e3e012325d
tanim arefin <ttanimarefin@gmail.com>
2023-12-12 14:56:33 +0600

    == , && condition logic

--- basic/variable.php
+++ basic/variable.php
@@ -130,6 +130,61 @@ echo"\n";
 //sprintf
 $output = sprintf("the number is %d and %d",$n1,$n2);
 echo $output;
+echo"\n";
+
+
+//condition logic and logical oparator
+
+$n4=10;
+if($n4%2==0){
+    echo "$n4 is even";
+}else{
+    echo "odd";
+}
+echo"\n";
+
+/*
+== eual
+!= not eual
+> grater
+< smaller
+*/
+
+
+
+$n5=20;
+$n6=12;
+if ($n5!=$n6){
+    echo "n5 is eual o n6";
+}else{
+    echo "nothing";
+}
+echo"\n";
+if($n5 >= $n6){
+    echo "$n5 has same amount or more amount of $n6";
+}
+elseif($n5 == $n6){
+    echo "$n5 has same amount of $n6";
+}elseif($n5>$n6){
+    echo "$n5 has more amount of $n6";
+}elseif($n5<$n6){
+    echo "$n5 has lass than amount of $n6";
+}else{
+    echo "no money";
+}
+echo"\n";
+
+$age= 20;
+if($age>=13 && $age <= 19){
+    echo "this person is teenager";
+}else{
+    echo "no teenager";
+}
+echo"\n";
+
+$food= "apple";
+if()
+
 
 
 
