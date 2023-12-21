f0efc873d271b80664bee1a156d01dc43d071e50
tanim arefin <ttanimarefin@gmail.com>
2023-12-21 12:39:16 +0600

    nasted if else

--- basic/variable.php
+++ basic/variable.php
@@ -182,8 +182,53 @@ if($age>=13 && $age <= 19){
 }
 echo"\n";
 
-$food= "apple";
-if()
+//leap year
+
+$year = 1996;
+
+if($year % 4==0 && $year % 100 == 0 && $year % 400 == 0){
+    echo "$year is leap year";
+}else if($year % 4==0 && $year % 100 == 0){
+    echo "$year is not leap year";
+}else if($year % 4==0){
+    echo "$year is leap year"; 
+}else{
+    echo " $year is not leap year ";
+}
+echo"\n";
+//nested loop
+
+$condition1=false;
+$condition2=false;
+$condition3=false;
+
+if($condition1){
+    if($condition2){
+        if($condition3){
+            echo "hello";
+        }else{
+            echo "no 1";
+
+        }
+    }else{
+        echo "no 2";
+    }
+}else{
+    echo "no 3";
+}
+
+//2nd way nested if else
+
+if($condition1 && $condition2 && $condition3){
+    echo "hello";
+}elseif($condition2 && $condition3){
+     echo"no 1";
+}elseif($condition2){
+    echo"no 2";
+}else{
+    echo "no 3";
+}
+
 
 
 
